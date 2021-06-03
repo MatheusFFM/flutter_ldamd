0d16b4705d5d091cf0bbfa1d420437c523c810f9
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-06-03 10:37:53 -0300

    feat: add maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyBbByy31cEtFnC0oELPrtaCTF5vfjGwEI8")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
